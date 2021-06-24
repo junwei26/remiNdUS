242311db3722118935aabc273dd7dcd05be2fd87
Teo Zhi Hao <teozhihao99@gmail.com>
2021-06-25 01:50:54 +0800

    Setup Reminder Packages API
    - Added support for getting all reminder packages in database

---
+++ backend/functions/src/controllers/reminderpackages.controller.js
@@ -0,0 +1,51 @@
+const admin = require("firebase-admin");
+const db = admin.firestore();
+
+exports.get = (req, res) => {
+  if (!req.query.uid) {
+    return res.status(400).send({ message: "You must be logged in to make this operation!" });
+  }
+
+  db.collection("users")
+    .where("uid", "==", req.query.uid)
+    .limit(1)
+    .get()
+    .then((data) => {
+      if (data.empty) {
+        return res.status(404).send({ message: "No user found. Please contact the administrator" });
+      }
+
+      let packageList = [];
+
+      // Actually only one doc (one user)
+      data.forEach((doc) => {
+        doc.ref
+          .collection("reminderpackages")
+          .get()
+          .then((data) => {
+            if (data.empty) {
+              return res.status(200).send({ message: "No Reminder Packages found." });
+            }
+
+            data.forEach((doc) => {
+              packageList.push(doc.data());
+            });
+
+            res.send(packageList);
+            return res.status(200).send({
+              message: "Reminder packages successfully retrieved!",
+            });
+          })
+          .catch((error) => {
+            return res.status(400).send({
+              message: `Error retrieving reminder packages. ${error}`,
+            });
+          });
+      });
+    })
+    .catch((error) => {
+      return res.status(400).send({
+        message: `Error retrieving user details. ${error}`,
+      });
+    });
+};
---
+++ backend/functions/src/routes/reminderpackages.routes.js
@@ -0,0 +1,11 @@
+module.exports = (app) => {
+  const user = require("../controllers/reminderpackages.controller.js");
+
+  var router = require("express").Router();
+
+  //   router.post("/create", user.create);
+
+  router.get("/", user.get);
+
+  app.use("/api/reminderpackages", router);
+};
