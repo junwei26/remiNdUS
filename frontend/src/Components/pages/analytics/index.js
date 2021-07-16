fa715c1f2ad2a4886e6fc90e86479174689e8e7e
junwei26 <limjunwei26@gmail.com>
2021-07-16 22:22:09 +0800

    Added bar chart for productivity analysis

--- frontend/src/Components/pages/analytics/index.js
+++ frontend/src/Components/pages/analytics/index.js
@@ -1,77 +1,220 @@
-import React from "react";
-import { Chart, PieSeries, Title, Tooltip } from "@devexpress/dx-react-chart-material-ui";
-import { Animation, HoverState, EventTracker } from "@devexpress/dx-react-chart";
-import { Grid } from "@material-ui/core";
-import localService from "../services/localService";
-import PropTypes from "prop-types";
+import PieChart from "./pieChart";
+import BarChart from "./barChart";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
+
 const DummyData = [
-  { name: "test 1", tag: "tag 1", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 2", tag: "tag 1", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 3", tag: "tag 2", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 4", tag: "tag 2", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 5", tag: "tag 3", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 6", tag: "tag 3", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 7", tag: "tag 4", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 8", tag: "tag 4", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 9", tag: "tag 4", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 10", tag: "tag 4", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 11", tag: "tag 5", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 12", tag: "tag 6", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 13", tag: "tag 6", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 14", tag: "tag 6", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 15", tag: "tag 6", startDateTime: "202101010000", endDateTime: "202101010500" },
-  { name: "test 16", tag: "tag 6", startDateTime: "202101010000", endDateTime: "202101010500" },
+  {
+    name: "test 1",
+    tag: "tag 1",
+    plannedStartDateTime: "202101010000",
+    plannedEndDateTime: "202101010500",
+    actualStartDateTime: "202101010000",
+    actualEndDateTime: "202101010500",
+  },
+  {
+    name: "test 2",
+    tag: "tag 1",
+    plannedStartDateTime: "202101010000",
+    plannedEndDateTime: "202101010500",
+    actualStartDateTime: "202101010000",
+    actualEndDateTime: "202101010500",
+  },
+  {
+    name: "test 3",
+    tag: "tag 2",
+    plannedStartDateTime: "202101010000",
+    plannedEndDateTime: "202101010500",
+    actualStartDateTime: "202101010000",
+    actualEndDateTime: "202101010500",
+  },
+  {
+    name: "test 4",
+    tag: "tag 2",
+    plannedStartDateTime: "202101020000",
+    plannedEndDateTime: "202101020500",
+    actualStartDateTime: "202101020000",
+    actualEndDateTime: "202101020500",
+  },
+  {
+    name: "test 5",
+    tag: "tag 3",
+    plannedStartDateTime: "202101030000",
+    plannedEndDateTime: "202101030500",
+    actualStartDateTime: "202101030000",
+    actualEndDateTime: "202101030500",
+  },
+  {
+    name: "test 6",
+    tag: "tag 3",
+    plannedStartDateTime: "202101030000",
+    plannedEndDateTime: "202101030500",
+    actualStartDateTime: "202101030000",
+    actualEndDateTime: "202101030500",
+  },
+  {
+    name: "test 7",
+    tag: "tag 4",
+    plannedStartDateTime: "202101030000",
+    plannedEndDateTime: "202101030500",
+    actualStartDateTime: "202101030000",
+    actualEndDateTime: "202101030500",
+  },
+  {
+    name: "test 8",
+    tag: "tag 4",
+    plannedStartDateTime: "202101030000",
+    plannedEndDateTime: "202101030500",
+    actualStartDateTime: "202101030000",
+    actualEndDateTime: "202101030500",
+  },
+  {
+    name: "test 9",
+    tag: "tag 4",
+    plannedStartDateTime: "202101010000",
+    plannedEndDateTime: "202101010500",
+    actualStartDateTime: "202101010000",
+    actualEndDateTime: "202101010500",
+  },
+  {
+    name: "test 10",
+    tag: "tag 4",
+    plannedStartDateTime: "202101010000",
+    plannedEndDateTime: "202101010500",
+    actualStartDateTime: "202101010000",
+    actualEndDateTime: "202101010500",
+  },
+  {
+    name: "test 11",
+    tag: "tag 5",
+    plannedStartDateTime: "202101010000",
+    plannedEndDateTime: "202101010500",
+    actualStartDateTime: "202101010000",
+    actualEndDateTime: "202101010500",
+  },
+  {
+    name: "test 12",
+    tag: "tag 6",
+    plannedStartDateTime: "202101010000",
+    plannedEndDateTime: "202101010500",
+    actualStartDateTime: "202101010000",
+    actualEndDateTime: "202101010500",
+  },
+  {
+    name: "test 13",
+    tag: "tag 6",
+    plannedStartDateTime: "202101010000",
+    plannedEndDateTime: "202101010500",
+    actualStartDateTime: "202101010000",
+    actualEndDateTime: "202101010500",
+  },
+  {
+    name: "test 14",
+    tag: "tag 6",
+    plannedStartDateTime: "202101010000",
+    plannedEndDateTime: "202101010500",
+    actualStartDateTime: "202101010000",
+    actualEndDateTime: "202101010500",
+  },
+  {
+    name: "test 15",
+    tag: "tag 6",
+    plannedStartDateTime: "202101010000",
+    plannedEndDateTime: "202101010500",
+    actualStartDateTime: "202101010000",
+    actualEndDateTime: "202101010500",
+  },
+  {
+    name: "test 16",
+    tag: "tag 6",
+    plannedStartDateTime: "202101010000",
+    plannedEndDateTime: "202101010500",
+    actualStartDateTime: "202101010000",
+    actualEndDateTime: "202101010500",
+  },
 ];
 
-const processActivityData = (activityArr) => {
-  const activityMap = new Map();
-  activityArr.forEach((activity) => {
-    if (activityMap.has(activity.tag)) {
-      const tagDuration = activityMap.get(activity.tag);
-      const currDuration =
-        new Date(localService.parseTime(activity.endDateTime)) -
-        new Date(localService.parseTime(activity.startDateTime));
-      activityMap.set(activity.tag, tagDuration + currDuration);
-    } else {
-      activityMap.set(
-        activity.tag,
-        new Date(localService.parseTime(activity.endDateTime)) -
-          new Date(localService.parseTime(activity.startDateTime))
-      );
-    }
-  });
-  const result = [];
-  activityMap.forEach((duration, tag) => result.push({ duration: duration / 36e5, tag }));
-  return result;
-};
-const dataArr = processActivityData(DummyData);
 const Analytics = () => {
-  const Content = ({ text, targetItem, ...restProps }) => {
-    return (
-      <Tooltip.Content
-        {...restProps}
-        text={`tag : ${dataArr[targetItem.point].tag} duration : ${text}`}
-        targetItem={targetItem}
-      />
-    );
+  const [active, setActive] = useState(1);
+  const SetView = (active) => {
+    setActive(active);
   };
-  Content.propTypes = {
-    text: PropTypes.string,
-    targetItem: PropTypes.object,
+
+  const ActiveView = () => {
+    switch (active) {
+      case "Activity breakdown by tag":
+        return <PieChart data={DummyData} />;
+      case "Productivity analysis":
+        return <BarChart data={DummyData} />;
+      default:
+        return <PieChart data={DummyData} />;
+    }
   };
+  const classes = useStyles();
+
   return (
-    <Grid container>
-      <Grid item xs>
-        <Chart data={dataArr}>
-          <PieSeries valueField="duration" argumentField="tag" innerRadius={0.6} />
-          <Title text="Breakdown of activities by tag" />
-          <Animation />
-          <EventTracker />
-          <Tooltip contentComponent={Content} />
-          <HoverState />
-        </Chart>
-      </Grid>
-    </Grid>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          <ListItem button key="Activity breakdown by tag">
+            <ListItemText
+              primary="Activity breakdown by tag"
+              onClick={() => SetView("Activity breakdown by tag")}
+            />
+          </ListItem>
+          <ListItem button key="Productivity analysis">
+            <ListItemText
+              primary="Productivity analysis"
+              onClick={() => SetView("Productivity analysis")}
+            />
+          </ListItem>
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <ActiveView />
+      </main>
+    </div>
   );
 };
 
---
+++ frontend/src/Components/pages/analytics/pieChart/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Chart, PieSeries, Title, Tooltip } from "@devexpress/dx-react-chart-material-ui";
+import { Animation, HoverState, EventTracker } from "@devexpress/dx-react-chart";
+import { Grid } from "@material-ui/core";
+import localService from "../../services/localService";
+import PropTypes from "prop-types";
+
+const PieChart = (props) => {
+  const processActivityData = (activityArr) => {
+    const activityMap = new Map();
+    activityArr.forEach((activity) => {
+      if (activityMap.has(activity.tag)) {
+        const tagDuration = activityMap.get(activity.tag);
+        const currDuration =
+          new Date(localService.parseTime(activity.actualEndDateTime)) -
+          new Date(localService.parseTime(activity.actualStartDateTime));
+        activityMap.set(activity.tag, tagDuration + currDuration);
+      } else {
+        activityMap.set(
+          activity.tag,
+          new Date(localService.parseTime(activity.actualEndDateTime)) -
+            new Date(localService.parseTime(activity.actualStartDateTime))
+        );
+      }
+    });
+    const result = [];
+    activityMap.forEach((duration, tag) => result.push({ duration: duration / 36e5, tag }));
+    return result;
+  };
+  const dataArr = processActivityData(props.data);
+  const Content = ({ text, targetItem, ...restProps }) => {
+    return (
+      <Tooltip.Content
+        {...restProps}
+        text={`tag : ${dataArr[targetItem.point].tag} duration : ${text}`}
+        targetItem={targetItem}
+      />
+    );
+  };
+  Content.propTypes = {
+    text: PropTypes.string,
+    targetItem: PropTypes.object,
+  };
+  return (
+    <Grid container>
+      <Grid item xs>
+        <Chart data={dataArr}>
+          <PieSeries valueField="duration" argumentField="tag" innerRadius={0.6} />
+          <Title text="Breakdown of activities by tag" />
+          <Animation />
+          <EventTracker />
+          <Tooltip contentComponent={Content} />
+          <HoverState />
+        </Chart>
+      </Grid>
+    </Grid>
+  );
+};
+
+PieChart.propTypes = {
+  data: PropTypes.array,
+};
+
+export default PieChart;
